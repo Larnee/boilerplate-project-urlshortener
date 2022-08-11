409b6f8409c6cc199a2c839cbcd8c3841b35b48d
larnee <larnee@mailfence.com>
2022-08-11 23:42:57 +0200

    Solution 1

--- index.js
+++ index.js
@@ -19,11 +19,33 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+const pairs=[];
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.post('/api/shorturl', function(req, res) {
-  res.json( req );
-	console.log("----------------------------------------");
-	console.log(req.body)
-	console.log("----------------------------------------");
+	const urlString = req.body.url;
+	const urlObject = new URL(urlString);
+	dns.lookup(urlObject.hostname, function onLookup(err, address, family) { 
+		if (err) res.json({ error: 'invalid url' });
+		else {
+			const alreadyExists = pairs.filter(p=>p.original_url===urlString);
+			if (alreadyExists.length>0) res.json(alreadyExists[0]);
+			else {
+				do {
+					let key = Math.floor(Math.random() * 20000);
+				} while (pairs.filter(p=>p.short_url===key).length>0);
+				const newPair={ original_url : urlString, short_url: key };
+				pairs.push(newPair);
+				res.json(newPair);
+			}
+		}
+	});
+});
+
+app.get('/api/shorturl/:key', function(req, res) {
+	const pair=pairs.filter(p=>p.short_url===req.params.key);
+	if(pair.length>0) res.redirect(pair[0].original_url); 
 });
 
 app.listen(port, function() {
